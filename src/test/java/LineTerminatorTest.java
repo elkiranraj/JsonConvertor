a7f7931f50f869ca80cb645454f088b1bfab5c48
KEruthayaraj <KEruthayaraj@KEURTHAYARAJ.casebank.com>
2019-08-21 17:35:06 -0400

    New changes

--- src/test/java/LineTerminatorTest.java
+++ src/test/java/LineTerminatorTest.java
@@ -6,7 +6,9 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.Collectors;
 
 public class LineTerminatorTest {
@@ -15,11 +17,32 @@ public class LineTerminatorTest {
 		LineTerminatorTest sol = new LineTerminatorTest();
 		System.out.println("Starting process..");
 		try {
-			sol.processFilesInSrcDir();
+			sol.printLines();
+//			sol.processFilesInSrcDir();
 			System.out.println("Process completed..!");
 		} catch (Exception e) {
 			System.out.println("Error processing file.");
 			e.printStackTrace();
+		}
+	}
+
+	private void printLines() throws Exception {
+		Path path = Paths.get("c:\\kiran\\test.txt");
+		// System.out.println("tagArray.add(\"" + l + "\");");
+		List<String> tagStream;
+		List<String> tailStream;
+		try (BufferedReader br = new BufferedReader(new FileReader(path.toFile()))) {
+			tagStream = br.lines().collect(Collectors.toList());
+		}
+		path = Paths.get("c:\\kiran\\fleet.txt");
+		try (BufferedReader br = new BufferedReader(new FileReader(path.toFile()))) {
+			tailStream = br.lines().collect(Collectors.toList());
+			Collections.sort(tailStream);
+		}
+		for (int i = 0; i < tailStream.size(); i++) {
+			int randomNum = ThreadLocalRandom.current().nextInt(0, tagStream.size());
+			System.out.println("INSERT INTO tail_tag(tag_name, serial_no) VALUES ('" + tagStream.get(randomNum) + "', '"
+					+ tailStream.get(i) + "');");
 		}
 	}
 
@@ -32,6 +55,7 @@ public class LineTerminatorTest {
 	}
 
 	private static final String NEWLINE_CHAR = "\\\\n";
+
 	private void processContent(List<String> lines, String fileName) throws IOException {
 		String output = "";
 		for (String l : lines) {
