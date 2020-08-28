4e40c321601c51f7bf519f073f85c2e89ddde8ee
KEruthayaraj <KEruthayaraj@KERUTHAYARAJ.casebank.com>
2020-08-28 10:45:23 -0400

    Conversion to Maven project

--- src/test/JsonToPojoConvertor.java
+++ src/test/JsonToPojoConvertor.java
@@ -3,6 +3,8 @@ package test;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
 import org.jsonschema2pojo.DefaultGenerationConfig;
 import org.jsonschema2pojo.GenerationConfig;
@@ -18,33 +20,48 @@ import com.sun.codemodel.JCodeModel;
 public class JsonToPojoConvertor {
 
 	public static void main(String[] args) {
-		String packageName = "com.test";
-		File inputJson = new File("." + File.separator + "PhoneInputVO.json");
-		File outputPojoDirectory = new File("." + File.separator + "convertedPojo");
+		String packageName = "com.casebank.rondo.solr.bean.result";
+		File inputJson = new File("." + File.separator + "defectJson.json");
+		File outputPojoDirectory = new File(
+				"." + File.separator + "convertedPojo");
 		outputPojoDirectory.mkdirs();
 		try {
-			new JsonToPojoConvertor().convert2JSON(inputJson.toURI().toURL(), outputPojoDirectory, packageName, inputJson.getName().replace(".json", ""));
+			new JsonToPojoConvertor().convert2JSON(inputJson.toURI().toURL(),
+					outputPojoDirectory, packageName,
+					inputJson.getName().replace(".json", ""));
 		} catch (IOException e) {
-			System.out.println("Encountered issue while converting to pojo: " + e.getMessage());
+			System.out.println("Encountered issue while converting to pojo: "
+					+ e.getMessage());
 			e.printStackTrace();
 		}
 	}
 
-	public void convert2JSON(URL inputJson, File outputPojoDirectory, String packageName, String className) throws IOException {
+	public void convert2JSON(URL inputJson, File outputPojoDirectory,
+			String packageName, String className) throws IOException {
+		className = "Defect";
 		JCodeModel codeModel = new JCodeModel();
 		URL source = inputJson;
 		GenerationConfig config = new DefaultGenerationConfig() {
 			@Override
-			public boolean isGenerateBuilders() { // set config option by overriding method
-				return true;
+			public boolean isGenerateBuilders() { // set config option by
+													// overriding method
+				return false;
 			}
 
 			@Override
 			public SourceType getSourceType() {
 				return SourceType.JSON;
 			}
+
+			@Override
+			public String getClassNameSuffix() {
+				return "SolrBean";
+			}
+
 		};
-		SchemaMapper mapper = new SchemaMapper(new RuleFactory(config, new Jackson2Annotator(config), new SchemaStore()), new SchemaGenerator());
+		SchemaMapper mapper = new SchemaMapper(new RuleFactory(config,
+				new Jackson2Annotator(config), new SchemaStore()),
+				new SchemaGenerator());
 		mapper.generate(codeModel, className, packageName, source);
 		codeModel.build(outputPojoDirectory);
 	}
